2bf5d274b81b57b295be3f1fe918506201c8ac35
hafizNabeel <iamnabeel800@gmail.com>
2023-05-30 11:45:56 +1000

    added Home screen with products & added images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container, Row, Col  } from 'react-bootstrap'
-
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
    <>
    <Header/>
    <main className='py-3'>
     <Container>
-        <h1>Welcome to react proshop</h1>
+        <HomeScreen/>
     </Container>
    </main>
    <Footer/>
